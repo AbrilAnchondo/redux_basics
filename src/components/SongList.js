9b87f8fea5d7f7e4b39827d04bc7e11b3dea53d7
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2021-07-25 17:38:49 -0400

    added and displayed SongDetail component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
   renderSongs () {
@@ -7,7 +8,12 @@ class SongList extends Component {
       return (
         <div className='item' key={song.title}>
           <div className='right floated content'>
-            <button className='ui button primary'>Select</button>
+            <button 
+              className='ui button primary'
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className='content'>
              {song.title}
@@ -28,7 +34,7 @@ const mapStateToProps = (state) => {
 //returning an obj that will show up as props inside songList component
   return { songs: state.songs };
 }
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
 
 // This is how the react-redux library works, always:
 // Import connect component at the top
